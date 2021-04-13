2304775eb71643b07621f1b12e8afb2d4348ba93
Miria Santos <miriaandressa1@gmail.com>
2021-04-13 10:36:52 -0300

    Camadas Security adicionadas

---
+++ armazemNatural/armazemNatural/src/main/java/com/cultivar/armazemNatural/model/UserLogin.java
@@ -0,0 +1,28 @@
+package com.cultivar.armazemNatural.model;
+
+public class UserLogin {
+		
+	private String email;	
+	private String senha;	
+	private String token;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+}
